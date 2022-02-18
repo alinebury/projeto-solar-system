57ead0bcc802740b2ecbc6f08387f94e3df58181
Aline Bury <alinebury@gmail.com>
2022-02-18 16:26:33 -0300

    Requisitos 2, 3, 4, e 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem />
+        <PlanetCard planetName="Terra" planetImage="path-to-image" />
+      </div>
     );
   }
 }
